4c7490fda433e358e83be5904f7093fdd4d5468e
Efremov Alexey <lexich121@gmail.com>
2016-08-03 23:01:17 +0300

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+### About wrr-applystyle
+[![Build Status](https://travis-ci.org/lexich/wrr-applystyle.svg)](https://travis-ci.org/lexich/wrr-applystyle)
+[![NPM version](https://badge.fury.io/js/wrr-applystyle.svg)](http://badge.fury.io/js/wrr-applystyle)
+[![Coverage Status](https://coveralls.io/repos/lexich/wrr-applystyle/badge.png?branch=master)](https://coveralls.io/r/lexich/wrr-applystyle?branch=master)
---
+++ index.js
@@ -0,0 +1,35 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+exports.default = function (Component, overwriteStyle) {
+  function WrapComponent(props) {
+    return _react2.default.createElement(Component, props);
+  }
+
+  var defaultProps = Component.defaultProps || {};
+  var style = defaultProps.style || {};
+
+  WrapComponent.propTypes = _extends({}, Component.propTypes);
+
+  if (!WrapComponent.propTypes.style) {
+    WrapComponent.propTypes.style = _react2.default.PropTypes.object.isRequired;
+  }
+
+  WrapComponent.defaultProps = _extends({}, defaultProps, {
+    style: _extends({}, style, overwriteStyle)
+  });
+  return WrapComponent;
+};
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = exports["default"];
